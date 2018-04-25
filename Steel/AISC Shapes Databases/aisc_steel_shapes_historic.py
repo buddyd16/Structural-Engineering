8abcb937c1aa40a24626fd1f78912268d1e11c67
buddyd16 <donnieb@gmail.com>
2018-04-25 13:01:34 -0400

    Add files via upload

--- Steel/AISC Shapes Databases/aisc_steel_shapes_historic.py
+++ Steel/AISC Shapes Databases/aisc_steel_shapes_historic.py
@@ -4,6 +4,21 @@ Created on Thu Apr 27 14:28:17 2017
 
 @author: DonB
 """
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import Tkinter as tk
 import tkFont
 
@@ -395,7 +410,7 @@ class Master_window:
         
 def main():
     root = tk.Tk()
-    root.title("AISC Historic - Shape Database")
+    root.title("AISC 14th Edition - Shape Database")
     app = Master_window(root)
     root.minsize(800,600)
     root.mainloop()
