f3e7c7e027e053910a9cacb88c1aff26debb5827
buddyd16 <donnieb@gmail.com>
2018-07-27 15:18:05 -0400

    license in gui
    added the license info to the gui

--- Steel/welds_gui.py
+++ Steel/welds_gui.py
@@ -85,6 +85,25 @@ class Master_window:
         self.nb_output_data.pack(fill=tk.BOTH, expand=1)
         
         # Input Notebooks
+        self.tab_license  = ttk.Frame(self.nb_inputs)
+        self.nb_inputs.add(self.tab_license , text='License')
+        
+        license_string = ("This program is free software; you can redistribute it and/or modify\n"
+                            "it under the terms of the GNU General Public License as published by\n"
+                            "the Free Software Foundation; either version 2 of the License, or\n"
+                            "(at your option) any later version.\n\n"
+
+                            "This program is distributed in the hope that it will be useful,\n"
+                            "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+                            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+                            "GNU General Public License for more details.\n\n"
+
+                            "You should have received a copy of the GNU General Public License along\n"
+                            "with this program; if not, write to the Free Software Foundation, Inc.,\n"
+                            "51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA")
+                            
+        tk.Label(self.tab_license, text=license_string,font=self.f_type, justify=tk.LEFT).grid(row=0, column=0)
+        
         self.tab_geometry  = ttk.Frame(self.nb_inputs)
         self.nb_inputs.add(self.tab_geometry , text='Weld Group Geometry - Input')
         
