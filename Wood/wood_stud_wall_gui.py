430100840464cb7615b2ce5f35b923318e04e022
buddyd16 <donnieb@gmail.com>
2018-11-28 10:48:24 -0500

    return focus
    return focus to main window after license dialog

--- Wood/wood_stud_wall_gui.py
+++ Wood/wood_stud_wall_gui.py
@@ -698,7 +698,27 @@ class Master_window:
         self.b_quit.pack(side=tk.RIGHT)
         self.b_save = tk.Button(self.base_frame,text="Save Inputs", command=self.save_inputs, font=helv, state = tk.DISABLED)
         self.b_save.pack(side=tk.RIGHT)
-            
+        
+        self.license_display()
+    
+    def license_display(self, *event):
+        license_string = ("This program is free software; you can redistribute it and/or modify\n"
+                    "it under the terms of the GNU General Public License as published by\n"
+                    "the Free Software Foundation; either version 2 of the License, or\n"
+                    "(at your option) any later version.\n\n"
+
+                    "This program is distributed in the hope that it will be useful,\n"
+                    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+                    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+                    "GNU General Public License for more details.\n\n"
+
+                    "You should have received a copy of the GNU General Public License along"
+                    "with this program; if not, write to the Free Software Foundation, Inc.,"
+                    "51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA\n"
+                    "\nA copy of the License can be viewed at:\n https://github.com/buddyd16/Structural-Engineering/blob/master/LICENSE")
+        tkMessageBox.showerror("License Information",license_string)
+        self.master.focus_force()
+        
     def quit_app(self):
         self.master.destroy()
         self.master.quit()
