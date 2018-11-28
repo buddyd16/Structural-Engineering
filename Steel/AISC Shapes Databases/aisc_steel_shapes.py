4f581bffe60f76ac938b2a39f6f8da4c071be23e
buddyd16 <donnieb@gmail.com>
2018-11-28 10:47:37 -0500

    return focus
    return focus to main window after license dialog

--- Steel/AISC Shapes Databases/aisc_steel_shapes.py
+++ Steel/AISC Shapes Databases/aisc_steel_shapes.py
@@ -196,6 +196,7 @@ class Master_window:
                     "51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA\n"
                     "\nA copy of the License can be viewed at:\n https://github.com/buddyd16/Structural-Engineering/blob/master/LICENSE")
         tkMessageBox.showerror("License Information",license_string)
+        self.master.focus_force()
         
     def value_filter_menu_switch(self, *event):
         option = self.value_filter.get()
