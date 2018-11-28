41272048681cb98b65ce8054ff07dd8427121ce0
buddyd16 <donnieb@gmail.com>
2018-11-28 10:16:07 -0500

    License Dialog
    GUI now displays the warranty waiver text from the GPL license

--- Concrete/concrete_T_beam_gui.py
+++ Concrete/concrete_T_beam_gui.py
@@ -24,6 +24,7 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import Tkinter as tk
 import tkFileDialog
+import tkMessageBox
 import math
 import tkFont
 import os
@@ -394,7 +395,25 @@ class Main_window:
 
         self.section_created = 0
         self.error_check = 0
+        self.license_display()
+    
+    def license_display(self, *event):
+        license_string = ("This program is free software; you can redistribute it and/or modify\n"
+                    "it under the terms of the GNU General Public License as published by\n"
+                    "the Free Software Foundation; either version 2 of the License, or\n"
+                    "(at your option) any later version.\n\n"
 
+                    "This program is distributed in the hope that it will be useful,\n"
+                    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+                    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+                    "GNU General Public License for more details.\n\n"
+
+                    "You should have received a copy of the GNU General Public License along"
+                    "with this program; if not, write to the Free Software Foundation, Inc.,"
+                    "51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA\n"
+                    "\nA copy of the License can be viewed at:\n https://github.com/buddyd16/Structural-Engineering/blob/master/LICENSE")
+        tkMessageBox.showerror("License Information",license_string)
+        
     def quit_app(self):
         self.master.destroy()
         self.master.quit()
