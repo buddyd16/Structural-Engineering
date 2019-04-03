cfe7b07d13d34a9c54531870c2c1c905ee35e089
DonB <donnieb@gmail.com>
2019-04-03 17:21:55 -0400

    License Update

--- Steel/welds_gui.py
+++ Steel/welds_gui.py
@@ -1,25 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 26 17:43:52 2018
-
-@author: DonB
-"""
-
-# -*- coding: utf-8 -*-
-
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+BSD 3-Clause License
+Copyright (c) 2019, Donald N. Bockoven III
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
 
 from __future__ import division
 import math
@@ -89,19 +92,21 @@ class Master_window:
         self.tab_license  = ttk.Frame(self.nb_inputs)
         self.nb_inputs.add(self.tab_license , text='License')
         
-        license_string = ("This program is free software; you can redistribute it and/or modify\n"
-                            "it under the terms of the GNU General Public License as published by\n"
-                            "the Free Software Foundation; either version 2 of the License, or\n"
-                            "(at your option) any later version.\n\n"
-
-                            "This program is distributed in the hope that it will be useful,\n"
-                            "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-                            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-                            "GNU General Public License for more details.\n\n"
-
-                            "You should have received a copy of the GNU General Public License along\n"
-                            "with this program; if not, write to the Free Software Foundation, Inc.,\n"
-                            "51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA")
+        license_string = (
+                            """Copyright (c) 2019, Donald N. Bockoven III\n
+                            All rights reserved.\n\n
+                            THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+                            AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+                            IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+                            DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+                            FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+                            DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+                            SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+                            CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+                            OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+                            OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n
+                            https://github.com/buddyd16/Structural-Engineering/blob/master/LICENSE"""
+                            )
                             
         tk.Label(self.tab_license, text=license_string,font=self.f_type, justify=tk.LEFT).grid(row=0, column=0)
         
